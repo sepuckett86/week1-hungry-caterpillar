bf0332ce97475c292e1e778a0e8826aad4992154
sepuckett86 <sepuckett86@gmail.com>
2019-05-01 10:13:10 -0700

    move function into separate file and wire up files

--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,7 @@
+import addCircleDiv from '../src/add-circle-div.js';
 const test = QUnit.test;
 
-function addCircleDiv(parent, color) {
-    const div = document.createElement('div');
-    div.classList.add('circle');
-    div.classList.add(color);
-    parent.appendChild(div);
-}
+
 
 test('adds div with circle and color class to dragon', assert => {
     // arrange
