d378b8348a1099b42441ac024b92919d165065b5
sepuckett86 <sepuckett86@gmail.com>
2019-05-01 09:55:48 -0700

    add test

---
+++ test/tests.js
@@ -0,0 +1,16 @@
+const test = QUnit.test;
+
+
+
+test('adds div with circle and color class to dragon', assert => {
+    // arrange
+    const circleClass = 'circle';
+    const colorClass = 'red';
+    const dragonParent = document.createElement('section');
+    const expected = '<div class="' + circleClass + ' ' + colorClass + '"></div>';
+    // act 
+    addCircleDiv(dragonParent, colorClass);
+    // assert
+    const actual = dragonParent.innerHTML();
+    assert.test(actual, expected);
+});
